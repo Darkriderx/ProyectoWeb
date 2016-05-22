0877327786e64e543c128f8bd3d5780c71a32478
LarzRS <larz_rs@hotmail.com>
2016-05-22 11:38:14 -0500

    Proyecto Web
    Proyecto TW

---
+++ src/java/proyecto/SessionTracking.java
@@ -0,0 +1,25 @@
+package proyecto;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SessionTracking extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        PrintWriter out = response.getWriter();
+        String UsName = request.getParameter("UsName");
+        String UsPass = request.getParameter("UsPass");
+        HttpSession sesion = request.getSession();
+        sesion.setAttribute(UsName, UsPass);
+        String id = sesion.getId();
+        out.println(id);
+        /*response.sendRedirect("mainPage.html");*/
+    }
+}
